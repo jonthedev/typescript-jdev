f3a3819732f93643a7645da43a51476bc449ea4a
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-24 18:46:45 +0200

    feat: add functions union type challenge with answer

---
+++ src/challenges/6-challenge-functions-ut-fp.ts
@@ -0,0 +1,27 @@
+/*
+
+? Functions - Using Union Types as Function Parameters
+
+* Your task is to create a function named processInput that accepts a parameter of a union type string | number. The function should behave as follows:
+
+* If the input is of type number, the function should multiply the number by 2 and log the result to the console.
+
+* If the input is of type string, the function should convert the string to uppercase and log the result to the console.
+
+*/
+
+const processInput = (value: string | number) => {
+  return typeof value === "number"
+    ? console.log(value * 2)
+    : console.log(value.toUpperCase())
+}
+
+processInput(10) // Output: 20
+processInput("Hello") // Output: HELLO
+
+/*
+
+* In this example, the processInput function takes a parameter input that can be either a string or a number. Inside the function, we use a type guard (typeof input === 'number') to check the type of input at runtime.
+
+* If input is a number, we double it. If input is a string, we convert it to uppercase.
+*/
