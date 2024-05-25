b0fd050e72e7aa3cdfef4ac2fdcd0aa014d4f202
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-25 12:30:56 +0200

    feat: add interface with methods lesson

---
+++ src/lesson/10.1-interface-with-methods.ts
@@ -0,0 +1,25 @@
+interface BookWithMethods {
+  readonly isbn: number
+  title: string
+  author: string
+  genre?: string
+  // method
+  printAuthor(): void
+  printTitle(message: string): string
+}
+
+const deepWork2: BookWithMethods = {
+  isbn: 9781455586691,
+  title: "Deep Work",
+  author: "Cal Newport",
+  genre: "Self-help",
+  printAuthor() {
+    console.log(this.author)
+  },
+  printTitle(message) {
+    return `${this.title} ${message}`
+  }
+}
+deepWork2.printAuthor()
+const result1 = deepWork2.printTitle("is an awesome book")
+console.log(result)
