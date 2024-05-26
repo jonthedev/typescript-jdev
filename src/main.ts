9a4a667d80f7895b65a3b742d7bf95a5f7579edb
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-26 16:58:37 +0200

    feat: add reverse mapping lesson to enums

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css"
 import typescriptLogo from "./typescript.svg"
 import viteLogo from "/vite.svg"
 import { setupCounter } from "./counter.ts"
-import "./challenges/7-challenge-interface.ts"
+import "./lesson/12-enums.ts"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
