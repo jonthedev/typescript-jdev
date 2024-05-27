4e21bcb4a393f81311c14686caf9a71213bf0eca
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-27 15:03:27 +0200

    feat: add fetch data zod lesson
    chore: add zod validation library

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css"
 import typescriptLogo from "./typescript.svg"
 import viteLogo from "/vite.svg"
 import { setupCounter } from "./counter.ts"
-import "./challenges/14-challenge-discriminated-unions.ts"
+import "./lesson/21-fetch-data-zod.ts"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
