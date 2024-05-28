c3fb92ea2b1a66c5f6fd56f9f1d4358ffaa436c5
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-28 14:21:40 +0200

    feat: add from data alternative example for form events

--- src/react-typescript-essentials/src/lessons/04-events/index.tsx
+++ src/react-typescript-essentials/src/lessons/04-events/index.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 
+type Person = {
+  name: string
+}
+
 function Component() {
   const [text, setText] = useState("")
   const [email, setEmail] = useState("")
@@ -9,10 +13,19 @@ function Component() {
     setEmail(e.target.value)
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // const formData = new FormData(e.currentTarget);
+    const formData = new FormData(e.target as HTMLFormElement)
+    const data = Object.fromEntries(formData)
+    const text = formData.get("text") as string
+    const person: Person = { name: data.text as string }
+  }
+
   return (
     <section>
       <h2>events example</h2>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <input
           className="form-input mb-1"
           type="text"
@@ -26,6 +39,7 @@ function Component() {
           className="form-input mb-1"
           value={email}
           onChange={handleChange}
+          name="email"
         />
         <button type="submit" className="btn btn-block">
           submit
