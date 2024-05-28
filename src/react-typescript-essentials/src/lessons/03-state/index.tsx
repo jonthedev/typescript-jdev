720a50e150e56c3f8568f32a142d5a26602d49be
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-05-28 13:28:34 +0200

    feat: add custom list example to state lesson

--- src/react-typescript-essentials/src/lessons/03-state/index.tsx
+++ src/react-typescript-essentials/src/lessons/03-state/index.tsx
@@ -7,10 +7,36 @@
 
 import { useState } from "react"
 
+type Link = {
+  id: number
+  url: string
+  text: string
+}
+
+const navLinks: Link[] = [
+  {
+    id: 1,
+    url: "https://reactjs.org",
+    text: "react docs"
+  },
+  {
+    id: 2,
+    url: "https://reactrouter.com",
+    text: "react router docs"
+  },
+  {
+    id: 3,
+    url: "https://reacttraining.com",
+    // remove text property to see the error
+    text: "react training"
+  }
+]
+
 function Component() {
   const [text, setText] = useState("shakeAndBake")
   const [number, setNumber] = useState(1)
   const [list, setList] = useState<string[]>([])
+  const [links, setLinks] = useState<Link[]>([])
 
   return (
     <div>
@@ -21,7 +47,9 @@ function Component() {
           // setText(1);
           // setNumber('hello');
           // setList([1, 3]);
-          setList(["hello", "world"])
+          // setList(["hello", "world"])
+          // setLinks([...links, { id: 4, url: 'hello', someValue: 'hello' }])
+          setLinks([...links, { id: 4, url: "hello", text: "hello" }])
         }}
       >
         click me
