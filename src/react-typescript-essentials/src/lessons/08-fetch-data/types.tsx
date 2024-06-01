4ce3ae7859225d21200cf7ac491f8c4fd6481af6
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-06-01 14:16:47 +0200

    refactor: implement react query with fetch data hook for tours data

--- src/react-typescript-essentials/src/lessons/08-fetch-data/types.tsx
+++ src/react-typescript-essentials/src/lessons/08-fetch-data/types.tsx
@@ -1,4 +1,7 @@
 import { z } from "zod"
+import axios from "axios"
+
+const url = "https://www.course-api.com/react-tours-project"
 
 export const tourSchema = z.object({
   id: z.string(),
@@ -9,3 +12,14 @@ export const tourSchema = z.object({
 })
 
 export type Tour = z.infer<typeof tourSchema>
+
+export const fetchTours = async (): Promise<Tour[]> => {
+  const response = await axios.get<Tour[]>(url)
+  const result = tourSchema.array().safeParse(response.data)
+
+  if (!result.success) {
+    console.log(result.error.message)
+    throw new Error(`Failed to parse tours`)
+  }
+  return result.data
+}
